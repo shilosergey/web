2e1396195324c9ab0ba57b19947e77d66ba37e74
shilosergey <shilosergeyshilo@gmail.ru>
2023-10-20 20:47:56 +0300

    123

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('index.html', name=name)
+    my_list=[{
+    "name":"Alex",
+    "age","42"
+    },
+    "name":"Serg",
+    "age","39"
+    }]
+    return render_template('index.html', name=name, my_list=my_list)
