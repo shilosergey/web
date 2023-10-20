b972816566c5175675d2badbf5f1ac45229c6771
shilosergey <shilosergeyshilo@gmail.ru>
2023-10-20 20:49:03 +0300

    123

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 def hello(name=None):
     my_list=[{
     "name":"Alex",
-    "age","42"
+    "age":"42"
     },
     "name":"Serg",
-    "age","39"
+    "age":"39"
     }]
     return render_template('index.html', name=name, my_list=my_list)
