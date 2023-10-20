7ce2a721b97fff4180b244ac09ec3cfdd2d4d52f
shilosergey <shilosergeyshilo@gmail.ru>
2023-10-20 20:31:42 +0300

    123

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('index.html', name=name)
